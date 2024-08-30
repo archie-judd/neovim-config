e34e1919d92788fe77d59c88fd5bf5de66b82190
archiejudd <archie.judd@yahoo.com>
2024-08-30 15:44:44 +0100

    make floating windows focusable

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -42,7 +42,7 @@ local config = function()
 	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 		border = "rounded",
 		silent = true,
-		focusable = false,
+		focusable = true,
 		max_height = 15,
 		max_width = 60,
 	})
@@ -50,7 +50,7 @@ local config = function()
 	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 		border = "rounded",
 		silent = true,
-		focusable = false,
+		focusable = true,
 		max_height = 15,
 		max_width = 65,
 	})
