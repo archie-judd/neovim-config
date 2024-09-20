ffe7e1adfa6586d6759a58af82185dbeeb331004
archie-judd <93987987+archie-judd@users.noreply.github.com>
2024-09-20 12:48:50 +0100

    Update lsp.lua

--- nvim/lua/utils/lsp.lua
+++ nvim/lua/utils/lsp.lua
@@ -6,7 +6,6 @@ function M.tsserver_organize_imports()
 		arguments = { vim.api.nvim_buf_get_name(0) },
 		title = "",
 	}
-	vim.print("Organizing imports for: " .. vim.inspect(params.arguments))
 	vim.lsp.buf.execute_command(params)
 end
 
