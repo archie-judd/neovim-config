4f54c14a28ae626ca6003398d483285cb96528f0
archiejudd <archie.judd@yahoo.com>
2024-10-10 15:29:16 +0100

    disable oil previews

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -82,15 +82,16 @@ function M.oil()
 			vim.o.colorcolumn = ""
 		end,
 	})
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "OilEnter",
-		callback = vim.schedule_wrap(function(args)
-			local oil = require("oil")
-			if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
-				oil.open_preview()
-			end
-		end),
-	})
+	-- Disabling preview until https://github.com/stevearc/oil.nvim/issues/435 is resolved
+	-- vim.api.nvim_create_autocmd("User", {
+	-- 	pattern = "OilEnter",
+	-- 	callback = vim.schedule_wrap(function(args)
+	-- 		local oil = require("oil")
+	-- 		if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+	-- 			oil.open_preview()
+	-- 		end
+	-- 	end),
+	-- })
 end
 
 function M.dap()
