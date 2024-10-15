202a533b58ae0dcc567ed487d3a186faed4ca4d3
archiejudd <archie.judd@yahoo.com>
2024-10-15 17:57:27 +0100

    use osc52 for clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
+local osc52 = require("vim.ui.clipboard.osc52")
+
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.o.splitright = true
@@ -7,3 +9,14 @@ vim.o.shortmess = "ctF" -- 'c' means 'don't show ins-completion mode messages', 
 vim.o.laststatus = 3 -- only one statusline
 vim.cmd.colorscheme("catppuccin-mocha")
 vim.api.nvim_set_hl(0, "NormalFloat", { fg = "none", bg = "none" }) -- fix oil background
+vim.g.clipboard = { -- OSC52 clipboard
+	name = "OSC 52",
+	copy = {
+		["+"] = osc52.copy("+"),
+		["*"] = osc52.copy("*"),
+	},
+	paste = {
+		["+"] = osc52.paste("+"),
+		["*"] = osc52.paste("*"),
+	},
+}
