f7bfb386361e6dd610389fd19873ca3a0028b38c
archiejudd <archie.judd@yahoo.com>
2024-10-22 21:05:38 +0100

    rm stray print

--- nvim/lua/utils/yank.lua
+++ nvim/lua/utils/yank.lua
@@ -2,13 +2,11 @@ M = {}
 
 function M.yank_filepath_to_system_register()
 	local file_path = vim.fn.expand("%:p")
-	vim.print(file_path)
 	vim.fn.setreg("+", file_path)
 end
 
 function M.yank_filename_to_system_register()
 	local file_name = vim.fn.expand("%:t")
-	vim.print(file_name)
 	vim.fn.setreg("+", file_name)
 end
 
