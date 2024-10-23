2688ac7e6526287d737156d69a5092f518b5367b
archiejudd <archie.judd@yahoo.com>
2024-10-23 10:57:47 +0100

    amend

--- nvim/lua/utils/yank.lua
+++ nvim/lua/utils/yank.lua
@@ -11,12 +11,12 @@ function M.yank_abs_filepath_to_system_register()
 end
 
 function M.yank_rel_filepath_to_unnamed_register()
-	local file_path = vim.fn.expand("%")
+	local file_path = vim.fn.expand("%:.")
 	vim.fn.setreg("", file_path)
 end
 
 function M.yank_rel_filepath_to_system_register()
-	local file_name = vim.fn.expand("%")
+	local file_name = vim.fn.expand("%:.")
 	vim.fn.setreg("+", file_name)
 end
 
