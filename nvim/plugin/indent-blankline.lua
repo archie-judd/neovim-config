e4bbdffff3c554211e7f0c20f408fd7f11ca3845
archiejudd <archie.judd@yahoo.com>
2024-11-06 15:24:17 +0000

    rm scope from indent-blankline

--- nvim/plugin/indent-blankline.lua
+++ nvim/plugin/indent-blankline.lua
@@ -1,19 +1,6 @@
 local config = function()
 	local ibl = require("ibl")
-	local hooks = require("ibl.hooks")
-	-- indent chars by thickness
-	-- `▏`
-	-- `▎`
-	-- `▍`
-	-- `▌`
-	-- `▋`
-	-- `▊`
-	-- `▉`
-	-- `█`
-	ibl.setup({ indent = { char = "▏" }, scope = { show_start = false, show_end = false } })
-	-- don't show or highlight the first indent
-	hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-	hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+	ibl.setup({ indent = { char = "▏" }, scope = { enabled = false } })
 end
 
 config()
