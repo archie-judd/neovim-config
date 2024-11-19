b86152cf484d23b868a51ea7f8967b7002953b6f
archiejudd <archie.judd@yahoo.com>
2024-11-19 13:02:22 +0000

    local_plugin util

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- vim.opt.runtimepath:prepend("../*") -- uncomment to load local plugins
+-- require("utils.local_plugin").load_local_plugin("/home/archie/workspaces/telescope-words.nvim") -- uncomment to load local plugins
 require("config.mappings").core()
 require("config.autocommands").core()
 require("config.options")
---
+++ nvim/lua/utils/local_plugin.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+---Add a local plugin to the runtimepath, prepending so that it takes precendence over any installed modules. This
+---should be used for development purposes only, and must be run at the top of init.lua
+---@param dir_path string
+function M.load_local_plugin(dir_path)
+	if vim.fn.isdirectory(dir_path) == 0 then
+		vim.notify("Directory does not exist: " .. dir_path, vim.log.levels.ERROR)
+		return
+	end
+	local dir_abs = vim.fn.fnamemodify(dir_path, ":p")
+	local parent = vim.fn.fnamemodify(dir_abs, ":h:h")
+	vim.opt.runtimepath:prepend(parent .. "/*")
+end
+
+return M
