71d2b5dc99371700ffa9ffbc6fe982bd4bc72771
archiejudd <archie.judd@yahoo.com>
2025-02-26 17:11:28 +0000

    improve ecb system prompt

--- nvim/plugin/codecompanion.lua
+++ nvim/plugin/codecompanion.lua
@@ -62,7 +62,7 @@ local function config()
 				prompts = {
 					{
 						role = "system",
-						content = "You are an experienced developer. Keep your responses concise and to the point. Don't include next-step suggestions.",
+						content = "You are an experienced developer. You will be requested to make some changes to a provided buffer. Do so. Keep your responses concise and to the point. Don't include next-step suggestions.",
 					},
 					{
 						role = "user",
