743e9262ffdd8f78ba62bd900cda4c9d29efdc57
archiejudd <archie.judd@yahoo.com>
2025-02-28 18:03:36 +0000

    project-specific formatters (2)

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -1,23 +1,10 @@
 local conform = require("conform")
 
-local function is_black_and_isort_installed()
-	local command = "which black > /dev/null 2>&1 && which isort > /dev/null 2>&1"
-	return os.execute(command) == 0
-end
-
-local function get_python_formatters()
-	if is_black_and_isort_installed() then
-		return { "isort", "black" }
-	else
-		return { "ruff" }
-	end
-end
-
 local config = function()
 	conform.setup({
 		formatters_by_ft = {
 			lua = { "stylua" },
-			python = get_python_formatters(),
+			python = { "isort", "black" },
 			javascript = { "prettierd" },
 			typescript = { "prettierd" },
 			typescriptreact = { "prettierd" },
