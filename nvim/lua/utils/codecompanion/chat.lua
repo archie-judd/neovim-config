44ea92e170190b97820c615729772ac36002b4a5
archiejudd <archie.judd@yahoo.com>
2025-03-04 09:45:47 +0000

    rm mini diff

--- nvim/lua/utils/codecompanion/chat.lua
+++ nvim/lua/utils/codecompanion/chat.lua
@@ -1,5 +1,4 @@
 local codecompanion = require("codecompanion")
-local diff = require("mini.diff")
 local utils = require("utils.core")
 
 local M = {}
@@ -47,30 +46,6 @@ function M.close()
 		local bufnr = chat.context.buffer
 		diff.disable(bufnr)
 		chat.ui:hide()
-	end
-end
-
-function M.jump_to_context_buffer()
-	local chat = codecompanion.last_chat()
-	if not chat or not chat.context or not chat.context.bufnr then
-		vim.notify("No active CodeCompanion context found", vim.log.levels.WARN)
-		return
-	end
-
-	local winnr = utils.get_winnr_for_bufnr(chat.context.bufnr)
-	if not winnr then
-		vim.notify("Buffer window not found", vim.log.levels.WARN)
-		return
-	end
-
-	vim.api.nvim_set_current_win(winnr)
-
-	local diff_info = diff.get_buf_data()
-	if diff_info and #diff_info.hunks > 0 then
-		-- go to the first hunk
-		diff.goto_hunk("first")
-		-- center the window
-		vim.api.nvim_command("normal! zz")
 	end
 end
 
