3d4a016a585f7d19c0eebee654ef28ea2fda87ef
archiejudd <archie.judd@yahoo.com>
2025-03-04 16:49:41 +0000

    fix maximise

--- nvim/lua/maximise.lua
+++ nvim/lua/maximise.lua
@@ -1,14 +1,19 @@
 local M = {}
 
 -- Variables to store the layout and maximised state
-local restore_cmd = nil
-local max_win_width = nil
-local max_win_height = nil
+local original_row = nil
+local original_col = nil
+local original_height = nil
+local original_width = nil
+local maximised_row = nil
+local maximised_col = nil
+local maximised_height = nil
+local maximised_width = nil
 
 function M.is_maximised()
 	local win_height = vim.api.nvim_win_get_height(0)
 	local win_width = vim.api.nvim_win_get_width(0)
-	if win_height == max_win_height and win_width == max_win_width then
+	if win_height == maximised_height and win_width == maximised_width then
 		return true
 	else
 		return false
@@ -16,19 +21,45 @@ function M.is_maximised()
 end
 
 function M.maximise()
-	restore_cmd = vim.fn.winrestcmd()
-	vim.cmd("vert resize | resize")
-	max_win_height = vim.api.nvim_win_get_height(0)
-	max_win_width = vim.api.nvim_win_get_width(0)
+	local MAX_HEIGHT = 0.975
+	local MAX_WIDTH = 0.975
+	local cols = vim.o.columns
+	local rows = vim.o.lines - 2
+
+	-- get the current configuration of the floating window
+	local win_config = vim.api.nvim_win_get_config(0)
+	original_row = win_config.row
+	original_col = win_config.col
+	original_height = win_config.height
+	original_width = win_config.width
+
+	-- update the row and col to reposition the window
+	maximised_row = math.floor(rows * (1 - MAX_HEIGHT) / 2)
+	maximised_col = math.floor(cols * (1 - MAX_WIDTH) / 2)
+	maximised_height = math.floor(rows * MAX_HEIGHT)
+	maximised_width = math.floor(cols * MAX_WIDTH)
+	win_config.row = maximised_row
+	win_config.col = maximised_col
+	win_config.height = maximised_height
+	win_config.width = maximised_width
+
+	-- apply the new configuration
+	vim.api.nvim_win_set_config(0, win_config)
 end
 
 function M.restore()
-	if restore_cmd then
-		vim.cmd(restore_cmd)
+	if M.is_maximised() then
+		local win_config = vim.api.nvim_win_get_config(0)
+		win_config.row = original_row
+		win_config.col = original_col
+		win_config.height = original_height
+		win_config.width = original_width
+		vim.api.nvim_win_set_config(0, win_config)
+		maximised_col = nil
+		maximised_row = nil
+		maximised_height = nil
+		maximised_width = nil
 	end
-	restore_cmd = nil
-	max_win_height = nil
-	max_win_width = nil
 end
 
 function M.toggle_maximise()
