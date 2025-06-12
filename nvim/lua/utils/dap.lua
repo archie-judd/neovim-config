b5c732aeb0e4b2549a83565a9cb0c629b12e98a8
archiejudd <archie.judd@yahoo.com>
2025-06-12 14:02:26 +0100

    close diffview when debugging

--- nvim/lua/utils/dap.lua
+++ nvim/lua/utils/dap.lua
@@ -1,5 +1,6 @@
 local core_utils = require("utils.core")
 local dap = require("dap")
+local diffview = require("diffview")
 local neotest = require("neotest")
 
 local M = {}
@@ -25,6 +26,8 @@ local function move_to_current_frame()
 end
 
 function M.start()
+	-- If diffview is open, close it before starting a debug session
+	diffview.close()
 	if not dap_is_active() then
 		vim.notify("Starting debug session", vim.log.levels.INFO)
 		vim.g.debugged_winnr = vim.api.nvim_get_current_win()
