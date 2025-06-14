ae40451cf4239abcf646af55b4c89ab3c0fc3bc1
archiejudd <archie.judd@yahoo.com>
2025-06-14 15:30:50 +0100

    add codecompanion overlay

--- flake.nix
+++ flake.nix
@@ -10,8 +10,51 @@
   outputs = { flake-utils, nixpkgs, nixpkgs-unstable, telescope-words, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+        # Get the most recent release of codecompanion.nvim (as of 2025-06-14)
+        overlay = final: prev: {
+          vimPlugins = prev.vimPlugins // {
+            codecompanion-nvim = prev.vimUtils.buildVimPlugin {
+              pname = "codecompanion.nvim";
+              version = "2025-06-14";
+              src = prev.fetchFromGitHub {
+                owner = "olimorris";
+                repo = "codecompanion.nvim";
+                rev = "a1a5431e7effefc83ee2422c633ac7ca11880bb5";
+                sha256 = "0h1wyv4l9dsbz2rsqsqjqxisa7k5l63l2b3y91g0kscybv1jy9hk";
+              };
+              dependencies = [ prev.vimPlugins.plenary-nvim ];
+              checkInputs = [
+                # Optional completion
+                prev.vimPlugins.blink-cmp
+                prev.vimPlugins.nvim-cmp
+                # Optional pickers
+                prev.vimPlugins.fzf-lua
+                prev.vimPlugins.mini-nvim
+                prev.vimPlugins.snacks-nvim
+                prev.vimPlugins.telescope-nvim
+              ];
+              nvimSkipModules = [
+                # Requires setup call
+                "codecompanion.actions.static"
+                "codecompanion.actions.init"
+                # Test
+                "minimal"
+              ];
+              meta.homepage =
+                "https://github.com/olimorris/codecompanion.nvim/";
+              meta.hydraPlatforms = [ ];
+            };
+          };
+        };
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+          overlays = [ overlay ];
+        };
 
         neovim = pkgs.callPackage ./neovim.nix {
           pkgs = pkgs;
