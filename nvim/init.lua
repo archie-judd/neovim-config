1e241d6d1ccf01fa8b4d927298348d6f08813b14
archiejudd <archie.judd@yahoo.com>
2025-06-24 22:36:43 +0100

    add blink-cmp-words

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,3 @@
--- require("utils.local_plugin").load_local_plugin("/home/archie/workspaces/telescope-words.nvim/") -- uncomment to load local plugins
 -- require("utils.local_plugin").load_local_plugin("/home/archie/workspaces/blink-cmp-words") -- uncomment to load local plugins
 require("config.mappings").core()
 require("config.autocommands").core()
--- nvim/plugin/blink-cmp.lua
+++ nvim/plugin/blink-cmp.lua
@@ -47,22 +47,43 @@ local function config()
 					module = "lazydev.integrations.blink",
 					score_offset = 100,
 				},
-				-- dictionary = {
-				-- 	name = "blink-cmp-words",
-				-- 	module = "blink-cmp-words.dictionary",
-				-- 	opts = {},
-				-- },
-				-- thesaurus = {
-				-- 	name = "blink-cmp-words",
-				-- 	module = "blink-cmp-words.thesaurus",
-				-- 	opts = {},
-				-- },
+				thesaurus = {
+					name = "blink-cmp-words",
+					module = "blink-cmp-words.thesaurus",
+					-- All available options
+					opts = {
+						-- A score offset applied to returned items.
+						-- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
+						score_offset = 0,
+
+						-- Default pointers define the lexical relations listed under each definition,
+						-- see Pointer Symbols below.
+						-- Default is as below ("antonyms", "similar to" and "also see").
+						pointer_symbols = { "!", "&", "^" },
+					},
+				},
+
+				-- Use the dictionary source
+				dictionary = {
+					name = "blink-cmp-words",
+					module = "blink-cmp-words.dictionary",
+					-- All available options
+					opts = {
+						-- The number of characters required to trigger completion.
+						-- Set this higher if completion is slow, 3 is default.
+						dictionary_search_threshold = 3,
+
+						-- See above
+						score_offset = 0,
+
+						-- See above
+						pointer_symbols = { "!", "&", "^" },
+					},
+				},
 			},
 			per_filetype = {
-				["dap-repl"] = { "dap" },
-				dap_watches = { "dap" },
-				-- text = { "thesaurus" },
-				-- markdown = { "thesaurus" },
+				text = { "dictionary", "thesaurus" },
+				markdown = { "dictionary", "thesaurus" },
 			},
 		},
 		appearance = { nerd_font_variant = "normal" },
