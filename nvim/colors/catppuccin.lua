9c0b563127eeb6fd1180608b654b8c8c141bf89d
archiejudd <archie.judd@yahoo.com>
2025-07-04 17:37:27 +0100

    make DiffText orange for catppuccin mocha

--- nvim/colors/catppuccin.lua
+++ nvim/colors/catppuccin.lua
@@ -1,7 +1,12 @@
 local catppuccin = require("catppuccin")
 
 local config = function()
-	catppuccin.setup({ flavour = "mocha" })
+	local flavour = "mocha" -- Default flavour
+	catppuccin.setup({ flavour = flavour })
+	if flavour == "mocha" then
+		-- Make inline diffs orange
+		vim.api.nvim_set_hl(0, "DiffText", { bg = "#604b49", fg = "#fab387" })
+	end
 end
 
 config()
