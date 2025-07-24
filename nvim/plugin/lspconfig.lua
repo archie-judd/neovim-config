fa57bc93a5bdde5f11073a8179b21936ae779698
archiejudd <archie.judd@yahoo.com>
2025-07-24 17:18:55 +0100

    add eslint

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -11,6 +11,7 @@ local config = function()
 	vim.lsp.enable("ts_ls")
 	vim.lsp.enable("sqlls")
 	vim.lsp.enable("yamlls")
+	vim.lsp.enable("eslint")
 	vim.lsp.enable("emmet_language_server")
 
 	vim.lsp.config("ts_ls", {
@@ -21,10 +22,26 @@ local config = function()
 					vim.lsp.buf.code_action({
 						context = {
 							only = { "source.organizeImports" },
+							diagnostics = {},
 						},
 						apply = true,
 					})
 				end,
+			})
+		end,
+	})
+
+	local base_on_attach = vim.lsp.config.eslint.on_attach
+	vim.lsp.config("eslint", {
+		on_attach = function(client, bufnr)
+			if not base_on_attach then
+				return
+			end
+
+			base_on_attach(client, bufnr)
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				buffer = bufnr,
+				command = "LspEslintFixAll",
 			})
 		end,
 	})
