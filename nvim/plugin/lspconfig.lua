b1f4bf138a6c383d11b08ed6e0aaf3e87aa7b482
archiejudd <archie.judd@yahoo.com>
2025-08-05 16:48:02 +0100

    fix typescript organise imports

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -16,16 +16,53 @@ local config = function()
 
 	vim.lsp.config("ts_ls", {
 		on_attach = function(client, bufnr)
+			-- This happens asynchronously, so we need to ensure we don't register the autocmd multiple times.
+			-- We also add a handler to write the file after organizing imports.
+			local organizing = false
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				buffer = bufnr,
 				callback = function()
-					vim.lsp.buf.code_action({
+					if organizing then
+						return
+					end
+
+					organizing = true
+					local range_params = vim.lsp.util.make_range_params(nil, "utf-8")
+					local params = {
+						textDocument = range_params.textDocument,
+						range = range_params.range,
 						context = {
 							only = { "source.organizeImports" },
 							diagnostics = {},
 						},
-						apply = true,
-					})
+					}
+					vim.lsp.buf_request(
+						bufnr,
+						"textDocument/codeAction",
+						params,
+						-- Callback handler to write the file after organizing imports
+						function(err, result, _)
+							if err then
+								organizing = false
+								pcall(vim.cmd.write)
+								return
+							end
+							if result then
+								for _, action in ipairs(result) do
+									if action.edit then
+										vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+									elseif action.command then
+										vim.lsp.buf_request_sync(0, "workspace/executeCommand", action.command)
+									end
+								end
+							end
+							organizing = false
+							vim.schedule(function()
+								pcall(vim.cmd.write)
+							end)
+						end
+					)
+					return true
 				end,
 			})
 		end,
