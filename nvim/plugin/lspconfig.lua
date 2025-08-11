a6ef148d19cf14dd168a422df4c9eaa0fd6b86bf
archiejudd <archie.judd@yahoo.com>
2025-08-11 09:48:56 +0100

    fix EslintFixAll

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -69,17 +69,21 @@ local config = function()
 		end,
 	})
 
-	local base_on_attach = vim.lsp.config.eslint.on_attach
 	vim.lsp.config("eslint", {
 		on_attach = function(client, bufnr)
-			if not base_on_attach then
-				return
-			end
-
-			base_on_attach(client, bufnr)
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				buffer = bufnr,
-				command = "LspEslintFixAll",
+				callback = function()
+					local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "eslint" })
+					if #clients > 0 then
+						vim.lsp.buf.code_action({
+							filter = function(action)
+								return action.kind and action.kind:match("^source%.fixAll%.eslint")
+							end,
+							apply = true,
+						})
+					end
+				end,
 			})
 		end,
 	})
