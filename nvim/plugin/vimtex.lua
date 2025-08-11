da0ff08d056216a4b540fc9227699732ed763116
archiejudd <archie.judd@yahoo.com>
2025-08-11 01:39:00 +0100

    fix vimtex

--- nvim/plugin/vimtex.lua
+++ nvim/plugin/vimtex.lua
@@ -1,5 +1,5 @@
 local config = function()
-	if vim.fn.executable("mac") == 1 then
+	if vim.fn.has("mac") == 1 then
 		vim.g.vimtex_view_method = "skim"
 	else
 		vim.g.vimtex_view_method = "zathura"
