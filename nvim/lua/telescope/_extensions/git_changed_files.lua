1334ff7ecf96e3f93b803fbe533079163368d9b9
archiejudd <archie.judd@yahoo.com>
2025-11-11 15:13:33 +0000

    fix git_changed_files

--- nvim/lua/telescope/_extensions/git_changed_files.lua
+++ nvim/lua/telescope/_extensions/git_changed_files.lua
@@ -10,14 +10,14 @@ local function git_changed_files()
 	local cmd = {
 		"bash",
 		"-c",
-		"git status --porcelain | cut -c4-",
+		"git status --porcelain -u | cut -c4-",
 	}
 
 	pickers
 		.new({}, {
 			prompt_title = "Git Changed Files",
 			finder = finders.new_oneshot_job(cmd, { entry_maker = make_entry.gen_from_file({ cwd = git_root }) }),
-			previewer = conf.values.grep_previewer({}),
+			previewer = conf.values.file_previewer({}),
 			sorter = conf.values.generic_sorter({}),
 			layout_config = {
 				width = 0.75,
