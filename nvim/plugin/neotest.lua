7d9893cde0c110343aa2c19667bbe12587f54f22
archiejudd <archie.judd@yahoo.com>
2025-11-21 20:24:18 +0000

    fix? neotest

--- nvim/plugin/neotest.lua
+++ nvim/plugin/neotest.lua
@@ -3,15 +3,18 @@ local neotest = require("neotest")
 local neotest_python = require("neotest-python")
 
 local config = function()
+	local python_path = vim.fn.exepath("python") ~= "" and vim.fn.exepath("python") or vim.fn.exepath("python3")
 	neotest.setup({
 		adapters = {
 			neotest_python({
 				dap = {
 					cwd = "${workspaceFolder}",
-					env = { PYTHONPATH = "${workspaceFolder}" },
+					env = { PYTHONPATH = "${workspaceFolder}", MPLBACKEND = "Agg" },
 					console = "integratedTerminal",
-					justMyCode = false, -- enable debugging of third party packages
+					justMyCode = true, -- enable debugging of third party packages
 				},
+				runner = "pytest",
+				python = python_path,
 			}),
 		},
 	})
