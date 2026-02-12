dff026b453be621908abbeeadad8463d8345e5a3
archiejudd <archie.judd@yahoo.com>
2026-02-12 11:53:39 +0000

    fix(maximise): fix for non floating windows

--- nvim/lua/lib/plugin/maximise.lua
+++ nvim/lua/lib/plugin/maximise.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 -- Variables to store the layout and maximised state
+---@type table<number, vim.api.keyset.win_config>
 local original_win_configs = {}
 local maximised = {}
 
@@ -11,15 +12,20 @@ local function maximise(win_id)
 	local rows = vim.o.lines - 2
 
 	-- get the current configuration of the floating window
-	original_win_configs[win_id] = vim.api.nvim_win_get_config(0)
-	local update_win_config = vim.api.nvim_win_get_config(0)
+
+	local update_win_config = vim.api.nvim_win_get_config(win_id)
+
 	if update_win_config.relative ~= "" then
+		original_win_configs[win_id] = vim.api.nvim_win_get_config(win_id)
 		-- update the row and col to reposition the window
 		update_win_config.row = math.floor(rows * (1 - FLOAT_MAX_HEIGHT) / 2)
 		update_win_config.col = math.floor(cols * (1 - FLOAT_MAX_WIDTH) / 2)
 		update_win_config.height = math.floor(rows * FLOAT_MAX_HEIGHT)
 		update_win_config.width = math.floor(cols * FLOAT_MAX_WIDTH)
 	else
+		for i, win_id_ in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+			original_win_configs[win_id_] = vim.api.nvim_win_get_config(win_id_)
+		end
 		update_win_config.height = vim.o.lines
 		update_win_config.width = vim.o.columns
 	end
@@ -29,8 +35,9 @@ local function maximise(win_id)
 end
 
 local function restore(win_id)
-	local original_win_config = original_win_configs[win_id]
-	vim.api.nvim_win_set_config(0, original_win_config)
+	for win_id_, config in pairs(original_win_configs) do
+		vim.api.nvim_win_set_config(win_id_, config)
+	end
 	maximised[win_id] = false
 	original_win_configs[win_id] = nil
 end
